238a6806b938e4cd0570b36d00444ee1160b1f86
Furkan <furkan5590@hotmail.com>
2024-02-05 17:27:20 +0300

    login page was added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex items-center justify-center min-h-screen bg-slate-400 ">
+        <div className="flex flex-col gap-y-5  py-10  px-8 border rounded-md shadow-lg bg-white "> 
+          <div>
+            <h1 className="text-4xl font-bold ">Hello User</h1>
+            <h3 className="text-gray-500 text-lg">Please enter your details</h3>
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <span>Email</span>
+            <input className="border-2 p-2 w-full rounded-sm " type="text" id="name" name="name" placeholder="@hotmail.com" />
+
+            <span>Password</span>
+            <input className="border-2 p-2 w-full rounded-sm " type="password" id="password" name="password" />
+          </div>
+          <div className="flex items-center justify-end font-bold">Forgot password?</div>
+
+          <button className="w-full bg-slate-400 p-2 rounded-md">Sign in</button>
+
+          <div className="flex items-center justify-center
+          ">
+            <span className="font-normal">
+              Don't have an account ? <span className="font-bold">Sign up for free</span>
+            </span>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
